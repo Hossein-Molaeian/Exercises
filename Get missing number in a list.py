1e31850943f189907a5617a6f778efc2f0ba2e03
Hossein Molaeian <96408257+Hossein7346@users.noreply.github.com>
2023-08-15 11:54:59 +0200

    Update Get missing number in a list.py

--- Get missing number in a list.py
+++ Get missing number in a list.py
@@ -1,7 +1,8 @@
 def get_missing_number(lst):
     return set(range(lst[len(lst)-1])[1:]) - set(l)
 
-
-l = list(range(1, 100))
-l.remove(10)
-print(get_missing_number(l))
+"""
+the first set contains all numbers from 1 to the last
+element of the input 'lst'. The set difference operation
+('-') is done to print the missing numbers in 'lst'
+"""
