0eebf408046abbde10a3af658791eddaa0e4670c
Hossein Molaeian <theniessoh@gmail.com>
2023-10-20 16:47:02 +0200

    Delete Simple Functions/Palindrome.py

--- Simple Functions/Palindrome.py
+++
@@ -1,8 +0,0 @@
-"""
-Simple: write a function that checks whether a word or phrase is palindrome
-(palindrome phrases read the same backwards as forwards)
-"""
-
-
-def is_palindrome(string):
-    return string.lower() == string.lower()[::-1]
