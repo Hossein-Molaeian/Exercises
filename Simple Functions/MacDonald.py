79d6a50356d9d9c107d247f947834ccf359ef76a
Hossein Molaeian <theniessoh@gmail.com>
2023-10-20 16:45:14 +0200

    Delete Simple Functions/MacDonald.py

--- Simple Functions/MacDonald.py
+++
@@ -1,9 +0,0 @@
-"""
-Simple: write a function that capitalizes the first and fourth letters of a string
-"""
-
-
-def macdonald(string):
-    if len(string) >= 4:
-        return string[:3].capitalize()+string[3:].capitalize()
-    return "Name is too short."
